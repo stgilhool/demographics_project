08e26f430432950ce9d832f5b1b024c246c7a177
gilhools@email.chop.edu <gilhools@email.chop.edu>
2019-06-19 16:53:40 -0400

    modified to accept cag data, update depricated tf stuff

--- dev/classifier.py
+++ dev/classifier.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import time, shutil, os
 
 import cag_input_data_mm as cdata
+import inspect_classes as inclass
 
 # Architecture
 n_hidden_1 = 50
@@ -11,9 +12,9 @@ input_dim = 2 # using 2-d ae encodings as input for now
 
 # Parameters
 learning_rate = 0.01
-training_epochs = 1000
-batch_size = 100
-display_step = 1
+training_epochs = 301
+batch_size = 1000
+display_step = 25
 
 def layer(input, weight_shape, bias_shape):
     weight_init = tf.random_normal_initializer(stddev=(2.0/weight_shape[0])**0.5)
@@ -22,7 +23,8 @@ def layer(input, weight_shape, bias_shape):
                         initializer=weight_init)
     b = tf.get_variable("b", bias_shape,
                         initializer=bias_init)
-    return tf.nn.relu(tf.matmul(input, W) + b)
+    #return tf.nn.relu(tf.matmul(input, W) + b)
+    return tf.nn.sigmoid(tf.matmul(input, W) + b)
 
 def inference(x):
     with tf.variable_scope("hidden_1"):
@@ -37,13 +39,13 @@ def inference(x):
     return output
 
 def loss(output, y):
-    xentropy = tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y)    
+    xentropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=y)    
     loss = tf.reduce_mean(xentropy)
     return loss
 
 def training(cost, global_step):
     tf.summary.scalar("cost", cost)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False, name='Adam')
     train_op = optimizer.minimize(cost, global_step=global_step)
     return train_op
 
@@ -56,6 +58,13 @@ def evaluate(output, y):
 
 if __name__ == '__main__':
     
+    val_size = 450
+    test_size = 1000
+    input_type = 'ae'
+    cag = inclass.read_data_classifier(input_type=input_type,
+                                       validation_size=val_size,
+                                       test_size=test_size)
+
     if os.path.exists("mlp_logs/"):
         shutil.rmtree("mlp_logs/")
 
@@ -84,7 +93,7 @@ if __name__ == '__main__':
             sess = tf.Session()
 
             summary_writer = tf.summary.FileWriter("mlp_logs/",
-                                                graph_def=sess.graph_def)
+                                                   graph_def=sess.graph_def)
 
             
             init_op = tf.global_variables_initializer()
@@ -98,10 +107,10 @@ if __name__ == '__main__':
             for epoch in range(training_epochs):
 
                 avg_cost = 0.
-                total_batch = int(mnist.train.num_examples/batch_size)
+                total_batch = int(cag.train.num_examples/batch_size)
                 # Loop over all batches
                 for i in range(total_batch):
-                    minibatch_x, minibatch_y = mnist.train.next_batch(batch_size)
+                    minibatch_x, minibatch_y = cag.train.next_batch(batch_size)
                     # Fit training using batch data
                     sess.run(train_op, feed_dict={x: minibatch_x, y: minibatch_y})
                     # Compute average loss
@@ -110,7 +119,7 @@ if __name__ == '__main__':
                 if epoch % display_step == 0:
                     print("Epoch:", '%04d' % (epoch+1), "cost =", "{:.9f}".format(avg_cost))
 
-                    accuracy = sess.run(eval_op, feed_dict={x: mnist.validation.images, y: mnist.validation.labels})
+                    accuracy = sess.run(eval_op, feed_dict={x: cag.validation.images, y: cag.validation.labels})
 
                     print("Validation Error:", (1 - accuracy))
 
@@ -123,6 +132,6 @@ if __name__ == '__main__':
             print("Optimization Finished!")
 
 
-            accuracy = sess.run(eval_op, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+            accuracy = sess.run(eval_op, feed_dict={x: cag.test.images, y: cag.test.labels})
 
             print("Test Accuracy:", accuracy)
