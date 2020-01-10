a1b122269bfddc96e16386ee2e1ec2a72ff9c3af
gilhools@email.chop.edu <gilhools@email.chop.edu>
2020-01-10 13:10:53 -0500

    classifier.py: major changes for ASHG poster. Added option to specify activation function, output confusion matrix, and more small changes

--- dev/classifier.py
+++ dev/classifier.py
@@ -7,24 +7,32 @@ import inspect_classes as inclass
 # Architecture
 n_hidden_1 = 50
 n_hidden_2 = 50
-output_dim = 2 # 2 classes for now: Caucasian & AA
-input_dim = 2 # using 2-d ae encodings as input for now
+#n_hidden_3 = 7
+output_dim = 3 # 2 classes for now: Caucasian & AA
+input_dim = 10 # using 2-d ae encodings as input for now
 
 # Parameters
 learning_rate = 0.01
-training_epochs = 301
-batch_size = 1000
+training_epochs = 1001
+batch_size = 10000
 display_step = 25
 
-def layer(input, weight_shape, bias_shape):
-    weight_init = tf.random_normal_initializer(stddev=(2.0/weight_shape[0])**0.5)
-    bias_init = tf.constant_initializer(value=0)
+def layer(input, weight_shape, bias_shape, activation='sigmoid'):
+    weight_init = tf.random_normal_initializer(stddev=(10.0/weight_shape[0])**0.5)
+    bias_init = tf.constant_initializer(value=0.1)
     W = tf.get_variable("W", weight_shape,
                         initializer=weight_init)
     b = tf.get_variable("b", bias_shape,
                         initializer=bias_init)
-    #return tf.nn.relu(tf.matmul(input, W) + b)
-    return tf.nn.sigmoid(tf.matmul(input, W) + b)
+    
+    if activation == 'sigmoid':
+        return tf.nn.sigmoid(tf.matmul(input, W) + b)
+    elif activation == 'relu':
+        return tf.nn.relu(tf.matmul(input, W) + b)
+    elif activation is None:
+        return tf.matmul(input,W) + b
+    else:
+        raise ValueError("Activation must be sigmoid, relu or None")
 
 def inference(x):
     with tf.variable_scope("hidden_1"):
@@ -32,14 +40,21 @@ def inference(x):
      
     with tf.variable_scope("hidden_2"):
         hidden_2 = layer(hidden_1, [n_hidden_1, n_hidden_2], [n_hidden_2])
+
+    #with tf.variable_scope("hidden_3"):
+     #   hidden_3 = layer(hidden_2, [n_hidden_2, n_hidden_3], [n_hidden_3])
      
     with tf.variable_scope("output"):
-        output = layer(hidden_2, [n_hidden_2, output_dim], [output_dim])
+        output_activation = None
+        output = layer(hidden_2, [n_hidden_2, output_dim], [output_dim], activation=output_activation)
+        #output = layer(hidden_3, [n_hidden_3, output_dim], [output_dim], activation=output_activation)
 
     return output
 
 def loss(output, y):
-    xentropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=y)    
+    xentropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=y)
+    #xentropy = tf.nn.weighted_cross_entropy_with_logits(y, output, pos_weight=0.95)
+    #xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=output, labels=y)    
     loss = tf.reduce_mean(xentropy)
     return loss
 
@@ -56,17 +71,32 @@ def evaluate(output, y):
     tf.summary.scalar("validation", accuracy)
     return accuracy
 
+def write_results(output, y):
+    with open('/home/gilhools/Rhythm_project/out/result_labels.txt','w') as r:
+        for entry in tf.map_fn(list(),tf.argmax(output,1)):
+            r.write(str(entry)+'\n')
+    with open('/home/gilhools/Rhythm_project/out/test_labels.txt','w') as t:
+        for entry in tf.map_fn(list(),tf.argmax(y,1)):
+            t.write(str(entry)+'\n')
+    return tf.argmax(output,1), tf.argmax(y,1)
+
+def write_results2(output, y):
+    return tf.argmax(output,1), tf.argmax(y,1)
+
+def confusion(output,y):
+    return tf.confusion_matrix(tf.argmax(y,1), tf.argmax(output,1))
+
 if __name__ == '__main__':
     
-    val_size = 450
-    test_size = 1000
-    input_type = 'ae'
+    val_size = 1000
+    test_size = 5000
+    input_type = 'pca'
     cag = inclass.read_data_classifier(input_type=input_type,
                                        validation_size=val_size,
                                        test_size=test_size)
 
-    if os.path.exists("mlp_logs/"):
-        shutil.rmtree("mlp_logs/")
+    if os.path.exists("mlp_logs"+str(output_dim)+"_classes/"):
+        shutil.rmtree("mlp_logs"+str(output_dim)+"_classes/")
 
     with tf.Graph().as_default():
 
@@ -88,11 +118,15 @@ if __name__ == '__main__':
 
             summary_op = tf.summary.merge_all()
 
+            write_op = write_results2(output, y)
+
+            confusion_op = confusion(output,y)
+
             saver = tf.train.Saver()
 
             sess = tf.Session()
 
-            summary_writer = tf.summary.FileWriter("mlp_logs/",
+            summary_writer = tf.summary.FileWriter("mlp_logs_"+str(output_dim)+"_classes/",
                                                    graph_def=sess.graph_def)
 
             
@@ -100,7 +134,7 @@ if __name__ == '__main__':
 
             sess.run(init_op)
 
-            # saver.restore(sess, "mlp_logs/model-checkpoint-66000")
+            # saver.restore(sess, "mlp_logs_7_classes/model-checkpoint-66000")
 
 
             # Training cycle
@@ -126,7 +160,7 @@ if __name__ == '__main__':
                     summary_str = sess.run(summary_op, feed_dict={x: minibatch_x, y: minibatch_y})
                     summary_writer.add_summary(summary_str, sess.run(global_step))
 
-                    saver.save(sess, "mlp_logs/model-checkpoint", global_step=global_step)
+                    saver.save(sess, "mlp_logs_"+str(output_dim)+"_classes/model-checkpoint", global_step=global_step)
 
 
             print("Optimization Finished!")
@@ -135,3 +169,13 @@ if __name__ == '__main__':
             accuracy = sess.run(eval_op, feed_dict={x: cag.test.images, y: cag.test.labels})
 
             print("Test Accuracy:", accuracy)
+
+            result_labels, test_labels = sess.run(write_op, feed_dict={x: cag.test.images, y: cag.test.labels})
+
+#            print(list(zip(result_labels,test_labels)))
+
+            confusion_matrix = sess.run(confusion_op, feed_dict={x: cag.test.images, y: cag.test.labels})
+
+            print("")
+            print(confusion_matrix)
+            
